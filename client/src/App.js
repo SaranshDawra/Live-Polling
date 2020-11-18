fa84134cf2d941d276678ceabe74630300520577
SaranshDawra <saransh2000dawra@gmail.com>
2020-11-18 19:56:27 +0530

    Routes updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
 
 const App = () => {
     return (
         <Router>
-            <Navbar />
+            
             <Switch>
                 <Route path="/" exact component={Home}/>
-                <Route path="/signin" exact />
-                <Route path="signup" exact />
+                <Route path="/signin" exact component={Login}/>
+                <Route path="/signup" exact component={Signup}/>
             </Switch>
         </Router>
     );
