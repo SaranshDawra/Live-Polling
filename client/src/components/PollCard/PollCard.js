658f7fdad005c07b0674c7c848ef14f59ed42668
SaranshDawra <saransh2000dawra@gmail.com>
2020-11-25 16:32:57 +0530

    PollCard component updated

--- client/src/components/PollCard/PollCard.js
+++ client/src/components/PollCard/PollCard.js
@@ -1,36 +1,67 @@
 import React from "react";
 import classes from "./pollcard.module.css";
 
+const Clickable = (props) => {
+    return (
+        <div className={classes.OptionCard} onClick={props.onClick}>
+            {props.option}
+            {
+                props.votes ? <div className={classes.Count}>{props.votes}</div> : null
+            }
+        </div>
+    );
+};
+
+const NonClickable = (props) => {
+    return (
+        <div className={classes.OptionCard}>
+            {props.option}
+            <div className={classes.Count}>{props.votes}</div>
+        </div>
+    );
+};
+
 const PollCard = (props) => {
     return (
         <div className={classes.Container}>
             <div className={classes.PollSection}>
-                <div className={classes.Question}>
-                    Laboris fugiat veniam ut anim mollit. Laboris fugiat veniam
-                    ut tempor anim mollit. Laboris fugiat veniam ut tempor anim
-                    mollit.
-                </div>
+                <div className={classes.Question}>{props.data.question}</div>
                 <div className={classes.OptionSection}>
-                    <div className={classes.OptionCard}>
-                        Pariatur reprehenderit amet est ex adipisicing nulla
-                        commodo.
-                        <div className={classes.Count}>
-                            205
-                        </div>
-                    </div>
-                    <div className={classes.OptionCard}>
-                        Deserunt culpa esse deserunt adipisicing eu velit id
-                        veniam et ea ea minim.
-                        <div className={classes.Count}>
-                            80
-                        </div>
-                    </div>
+                    {props.clickable ? (
+                        <>
+                            <Clickable
+                                option={props.data.options[0]}
+                                votes={props.data.votes ? props.data.votes[0] : null}
+                                onClick={() => props.clicked(0)}
+                            />
+                            <Clickable
+                                option={props.data.options[1]}
+                                votes={props.data.votes ? props.data.votes[1] : null}
+                                onClick={() => props.clicked(1)}
+                            />
+                        </>
+                    ) : (
+                        <>
+                            <NonClickable
+                                option={props.data.options[0]}
+                                votes={props.data.votes[0]}
+                            />
+                            <NonClickable
+                                option={props.data.options[1]}
+                                votes={props.data.votes[1]}
+                            />
+                        </>
+                    )}
                 </div>
             </div>
-            <div className={classes.Line}></div>
-            <div className={classes.Btn}>
-                <div className={classes.DeleteBtn}>DELETE POLL</div>
-            </div>
+            {props.delete && (
+                <>
+                    <div className={classes.Line}></div>
+                    <div className={classes.Btn}>
+                        <div className={classes.DeleteBtn}>DELETE POLL</div>
+                    </div>
+                </>
+            )}
         </div>
     );
 };
