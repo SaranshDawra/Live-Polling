6b7981f2cc29dfe7a4c396aae848469f53a78d1b
SaranshDawra <saransh2000dawra@gmail.com>
2020-12-01 19:02:50 +0530

    deployment fixes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require('path');
 const users = require("./routes/users");
 const polls = require("./routes/polls");
 require("dotenv").config();
@@ -22,6 +23,13 @@ mongoose
 
 app.use("/api/user", users);
 app.use("/api/polls", polls);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
